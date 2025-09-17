b843b5422a7cc60efe8323cac35c1ef0781b4a61
Kyrylo Likhachov <4emploxmouhik@gmail.com>
2025-09-17 22:05:45 +0300

    fix: use relative path for runtime resources

--- Koshenya/Properties/AssemblyInfo.cs
+++ Koshenya/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("d97312b3-7f87-42fd-b486-8dfd3fbf7fc6")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
